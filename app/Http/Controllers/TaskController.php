57cf7e8c3dd1decedc0188bb9260ca74d0e749f0
Leonardo Araujo <leoaraujofnd@gmail.com>
2025-08-17 23:36:18 -0300

    refactir: Enhance task update response with detailed task data

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -197,7 +197,19 @@ class TaskController extends Controller
         return response()->json([
             'success' => true,
             'message' => 'Task updated successfully',
-            'data' => $task,
+            'data' => [
+                'id' => $task->id,
+                'title' => $task->title,
+                'description' => $task->description,
+                'totalPomodori' => $task->totalPomodori,
+                'pomodoroValue' => $task->pomodoroValue,
+                'completedPomodori' => $task->completedPomodori,
+                'status' => $task->status,
+                'taskDate' => $task->taskDate,
+                'dueDate' => $task->dueDate,
+                'assignedAt' => $task->assigned_at,
+                'completedAt' => $task->completed_at,
+            ],
         ], Response::HTTP_OK);
     }
 
